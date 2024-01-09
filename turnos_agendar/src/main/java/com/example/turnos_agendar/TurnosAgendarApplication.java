c0ac3cc788094b7e025d34c5dab694d6353ef04f
BorgoValentino <borgovalentino1@gmail.com>
2024-01-09 12:17:37 -0300

    Problems with the conection btw server and client

--- turnos_agendar/src/main/java/com/example/turnos_agendar/TurnosAgendarApplication.java
+++ turnos_agendar/src/main/java/com/example/turnos_agendar/TurnosAgendarApplication.java
@@ -2,8 +2,12 @@ package com.example.turnos_agendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = "com.example.turnos_agendar.Configuración")
 public class TurnosAgendarApplication {
 
 	public static void main(String[] args) {
